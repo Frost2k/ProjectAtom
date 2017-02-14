f3ee688d7c54a767a5e52916576fddec71478df6
James Scott <James Scott>
2017-02-14 15:06:36 -0600

    First commit

---
+++ Voice Recognition/Form1.Designer.cs
@@ -0,0 +1,90 @@
+﻿namespace Voice_Recognition
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.btnEnable = new System.Windows.Forms.Button();
+            this.btnDisable = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 12);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(389, 254);
+            this.textBox1.TabIndex = 0;
+            this.textBox1.Text = "-Log-";
+            // 
+            // btnEnable
+            // 
+            this.btnEnable.Location = new System.Drawing.Point(12, 272);
+            this.btnEnable.Name = "btnEnable";
+            this.btnEnable.Size = new System.Drawing.Size(170, 23);
+            this.btnEnable.TabIndex = 1;
+            this.btnEnable.Text = "Enable Voice Control";
+            this.btnEnable.UseVisualStyleBackColor = true;
+            this.btnEnable.Click += new System.EventHandler(this.btnEnable_Click);
+            // 
+            // btnDisable
+            // 
+            this.btnDisable.Enabled = false;
+            this.btnDisable.Location = new System.Drawing.Point(304, 272);
+            this.btnDisable.Name = "btnDisable";
+            this.btnDisable.Size = new System.Drawing.Size(97, 23);
+            this.btnDisable.TabIndex = 2;
+            this.btnDisable.Text = "Disable Voice Control";
+            this.btnDisable.UseVisualStyleBackColor = true;
+            this.btnDisable.Click += new System.EventHandler(this.btnDisable_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(413, 307);
+            this.Controls.Add(this.btnDisable);
+            this.Controls.Add(this.btnEnable);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Speech Recognition Tutorial";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button btnEnable;
+        private System.Windows.Forms.Button btnDisable;
+    }
+}
+
---
+++ Voice Recognition/Form1.cs
@@ -0,0 +1,215 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Speech.Recognition;
+using System.Speech.Synthesis;
+
+namespace Voice_Recognition
+{
+    public partial class Form1 : Form
+    {
+        SpeechRecognitionEngine recEngine = new SpeechRecognitionEngine();
+        SpeechSynthesizer synthesizer = new SpeechSynthesizer();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Choices commands = new Choices();
+            commands.Add(new string[]{"hi",
+                // numbers segment
+                "one","two","three","four","five","six",
+                "seven","eight","nine","zero","eleven","twelve","thirteen","fourteen","fifteen","sixteen","seventeen","eighteen","nineteen","twenty",
+                "twenty one","twenty two","twenty three","twenty four","twenty five","twenty six","twenty seven","twenty eight",
+                "twenty nine","thirty","thirty one","thirty two","thirty three","thirty four","thirty five","thirty six","thirty seven","thirty eight",
+                "thirty nine","fourty","through",
+                // atom commands for clearing and exiting
+                "atom clear the line","atom end the session","atom delete all","send",
+                // atom commands for displaying stucture
+                "atom show the sticks","atom show the lines","atom show the cartoon","show the ribbon","atom hide the lines",
+                "atom hide the sticks","atom hide the cartoon","atom hide the ribbon",
+                "atom enhance residues","atom isolate residues","through",
+                "atom color the selection blue","atom color the selection green","atom color the selection purple","atom color the selection red",
+                "atom color the selection yellow",
+                //fetch commands
+                "atom fetch","adom fetch","four oxy bound hemagloben","adom fetch hemagloben"
+                });
+            GrammarBuilder gBuilder = new GrammarBuilder();
+            gBuilder.Append(commands);
+            Grammar grammar = new Grammar(gBuilder);
+
+            recEngine.LoadGrammarAsync(grammar);
+            recEngine.SetInputToDefaultAudioDevice(); // Check for kinect audio device 
+            recEngine.SpeechRecognized += RecEngine_SpeechRecognized;
+            
+        }
+
+        private void RecEngine_SpeechRecognized(object sender, SpeechRecognizedEventArgs e)
+        {
+            switch(e.Result.Text)
+            {
+                case ("atom clear the line"):
+                    SendKeys.Send("^a,{BACKSPACE}");
+                    break;
+                //case "say hello":
+                //    PromptBuilder builder = new PromptBuilder();
+
+                //    builder.StartSentence();
+                //    builder.AppendText("Hello Miguel");
+                //    builder.EndSentence();
+
+                //    builder.AppendBreak(new TimeSpan(0,0,0,0,1));
+
+                //    builder.StartSentence();
+                //    builder.AppendText("how are you", PromptEmphasis.Strong);
+                //    builder.EndSentence();
+                //    synthesizer.SpeakAsync(builder);
+                //    break;
+                //case "speak selected text":
+                //    synthesizer.SpeakAsync(textBox1.SelectedText);
+                //    break;
+                case "one":
+                    SendKeys.Send("1");
+                    break;
+                case "two":
+                    SendKeys.Send("2");
+                    break;
+                case "three":
+                    SendKeys.Send("3");
+                    break;
+                case "four":
+                    SendKeys.Send("4");
+                    break;
+                case "five":
+                    SendKeys.Send("5");
+                    break;
+                case "six":
+                    SendKeys.Send("6");
+                    break;
+                case "seven":
+                    SendKeys.Send("7");
+                    break;
+                case "eight":
+                    SendKeys.Send("8");
+                    break;
+                case "nine":
+                    SendKeys.Send("9");
+                    break;
+                case "zero":
+                    SendKeys.Send("0");
+                    break;
+                case "adom fetch four oxy bound hemagloben":
+                    SendKeys.Send("fetch 1gzx");
+                    SendKeys.Send("{ENTER}");
+                    break;
+                case "adom fetch hemagloben":
+                    SendKeys.Send("fetch 1a3n");
+                    SendKeys.Send("{ENTER}");
+                    break;
+                case "send":
+                    SendKeys.Send("{ENTER}");
+                    break;
+                case "atom hide the lines":
+                    SendKeys.Send("hide lines");
+                    SendKeys.Send("{ENTER}");
+                    break;
+                case "atom show the lines":
+                    SendKeys.Send("show lines");
+                    SendKeys.Send("{ENTER}");
+                    break;
+                case "atom hide the sticks":
+                    SendKeys.Send("hide sticks");
+                    SendKeys.Send("{ENTER}");
+                    break;
+                case "atom show the sticks":
+                    SendKeys.Send("show sticks");
+                    SendKeys.Send("{ENTER}");
+                    break;
+                case "atom hide the cartoon":
+                    SendKeys.Send("hide cartoon");
+                    SendKeys.Send("{ENTER}");
+                    break;
+                case "atom show the cartoon":
+                    SendKeys.Send("show cartoon");
+                    SendKeys.Send("{ENTER}");
+                    break;
+                case "atom hide the ribbon":
+                    SendKeys.Send("hide ribbon");
+                    SendKeys.Send("{ENTER}");
+                    break;
+                case "atom show the ribbon":
+                    SendKeys.Send("show ribbon");
+                    SendKeys.Send("{ENTER}");
+                    break;
+                case "atom enhance residues":
+                    SendKeys.Send("zoom resi ");
+                    break;
+                case "residues":
+                    SendKeys.Send("resi ");
+                    break;
+                case "through":
+                    SendKeys.Send("-");
+                    break;
+                case "atom isolate residues":
+                    SendKeys.Send("select resi ");
+                    break;
+                case "atom end session":
+                    SendKeys.Send("quit ");
+                    SendKeys.Send("{ENTER}");
+                    break;
+                case "atom color the selection purple":
+                    SendKeys.Send("color purple, sele");
+                    SendKeys.Send("{ENTER}");
+                    break;
+                case "atom color the selection yellow":
+                    SendKeys.Send("color yellow, sele ");
+                    SendKeys.Send("{ENTER}");
+                    break;
+                case "atom color the selection red":
+                    SendKeys.Send("color red, sele ");
+                    SendKeys.Send("{ENTER}");
+                    break;
+                case "atom color the selection blue":
+                    SendKeys.Send("color blue, sele ");
+                    SendKeys.Send("{ENTER}");
+                    break;
+                case "atom color the selection green":
+                    SendKeys.Send("color green, sele ");
+                    SendKeys.Send("{ENTER}");
+                    break;
+                case "selection":
+                    SendKeys.Send("sele");
+                    SendKeys.Send("{ENTER}");
+                    break;
+                case "atom delete all":
+                    SendKeys.Send("delete all");
+                    SendKeys.Send("{ENTER}");
+                    break;
+
+
+            }
+        }
+
+        private void btnEnable_Click(object sender, EventArgs e)
+        {
+            recEngine.RecognizeAsync(RecognizeMode.Multiple);
+            btnDisable.Enabled = true;
+            btnEnable.Enabled = false;
+        }
+
+        private void btnDisable_Click(object sender, EventArgs e)
+        {
+            recEngine.RecognizeAsyncStop();
+            btnDisable.Enabled = false;
+            btnEnable.Enabled = true;
+        }
+    }
+}
